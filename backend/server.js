a80f05273e0b422001517518e6d9b6bb781ead9a
Jirro Aeron Guiao <79578145+kkazei@users.noreply.github.com>
2025-03-20 20:42:54 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -44,10 +44,11 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/maintenance", maintenanceRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
+    const frontendBuildPath = path.resolve(__dirname, "../frontend/dist");
+    app.use(express.static(frontendBuildPath));
+    
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.join(frontendBuildPath, "index.html"));
     });
 }
 
